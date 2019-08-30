aeacd426a0afeb8d8434d58036416fd017437ea9
davidfung <davidfung@amgcomputing.com>
2019-08-29 23:38:25 -0700

    Add radio buttons in settings

--- lib/pages/settings.dart
+++ lib/pages/settings.dart
@@ -1,11 +1,70 @@
 import 'package:flutter/material.dart';
 
-class SettingsPage extends StatelessWidget {
+const int DEFAULT_PIN_LENGTH = 6;
+
+class SettingsPage extends StatefulWidget {
+  @override
+  _SettingsPageState createState() => _SettingsPageState();
+}
+
+class _SettingsPageState extends State<SettingsPage> {
+  int _pinLength = DEFAULT_PIN_LENGTH;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text('Settings'),),
-      body: Center(child: Text("Settings Page"),),
+      appBar: AppBar(
+        title: Text('Settings'),
+      ),
+      body: _buildPage(context),
+    );
+  }
+
+  Widget _buildPage(BuildContext context) {
+    return Center(
+      child: Column(
+        children: <Widget>[
+          Text('Pin Length'),
+          ListTile(
+            title: const Text('4 digits'),
+            leading: Radio(
+              value: 4,
+              groupValue: _pinLength,
+              onChanged: (int value) {
+                setState(() {
+                  _pinLength = value;
+                });
+              },
+            ),
+          ),
+          ListTile(
+            title: const Text('6 digits'),
+            leading: Radio(
+              value: 6,
+              groupValue: _pinLength,
+              onChanged: (int value) {
+                setState(() {
+                  _pinLength = value;
+                  print(_pinLength);
+                });
+              },
+            ),
+          ),
+          ListTile(
+            title: const Text('8 digits'),
+            leading: Radio(
+              value: 8,
+              groupValue: _pinLength,
+              onChanged: (int value) {
+                setState(() {
+                  _pinLength = value;
+                  print(_pinLength);
+                });
+              },
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
